4f501f0fc55889cef9a429564ea8122e5f8cca56
NovaCore <novacore89@gmail.com>
2019-05-13 17:57:27 -0700

    Add assignment 39

---
+++ Assignment 36/A36.js
@@ -0,0 +1,25 @@
+//var randomnumber = Math.floor((Math.random()*10));
+var randomnumber = 11;
+switch (randomnumber) {
+    case 0:
+    case 1:
+    case 2:
+    console.log("The random number is ${randomnumber}.");
+    break;
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    console.log("The random number is between 3 and 7.");
+    break;
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    console.log("The random number is greater than 7.");
+    case 11:
+    console.log(5 + 5)
+    break;
+    default:
+    console.log("Something went wrong...");
+}
---
+++ Assignment 39/A39.js
@@ -0,0 +1,5 @@
+for(i=0; i<= 10000; i++) {
+  if(i % 2 == 0)   {
+    document.write(i+"<br>")
+  }
+}
